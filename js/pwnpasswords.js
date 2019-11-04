eab9ebf25e9840863e2f0e79d8a24b612ed263d1
Huasoroa <huasoroa@gmail.com>
2019-11-04 12:39:57 +0100

    Oplossing Taak Ehb

--- js/pwnpasswords.js
+++ js/pwnpasswords.js
@@ -1,10 +1,28 @@
 
   const checkPassword = async ()=>{
     let password = document.getElementById('password').value;
-
-    const response = await fetch('https://api.pwnedpasswords.com/pwnedpassword/'+password)
-    const myJson = await response.json();
-    alert("Json:"+myJson);
+    let hash =  new Hashes.SHA1().hex(password);
+    console.log("Password:"+password+", Hash: "+hash+", substring: "+hash.substring(0,5));
+    const response = await fetch('https://api.pwnedpasswords.com/range/'+hash.substring(0,5))
+    console.log("Response: "+response);
+    const data = await response.text();
+    console.log("Data : ",data);
+    var array = data.split("\r\n");
+    var suffix = hash.substring(5,40).toUpperCase();
+    var count=0;
+    for (var i = 0; i < array.length; i++) {
+      let f = array[i].split(":");
+      if (f[0]===suffix) {
+        var b = parseInt(f[1]);
+          count = count + b;
+      }
+    }
+    if (count<300) {
+      alert("Password has been breached "+count+" times.\nIt is safe enough");
+    } else if (count>=300) {
+      alert("Password has been breached "+count+" times.\nPassword is unsafe choose another password");
+    }
+    console.log("count : "+count)
   }
 
     document.getElementById("myBtn")
